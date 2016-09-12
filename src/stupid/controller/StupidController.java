d1856a3cf8eb04643abde11b8bffdf3c3f25bb85
Jake Nilson <jnil0572@740s-pl-132.local>
2016-09-12 12:26:20 -0600

    Added questions for assignment.

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -24,6 +24,23 @@ public class StupidController
 	{
 		System.out.println("What is your name?");
 		String userInput = readKeyboardInput.nextLine();
-		System.out.println("You said: " + userInput);
+		System.out.println("Nice to meet you " + userInput + "!");
+		
+		System.out.println("How old are you " + userInput + "?");
+		Double intInput = readKeyboardInput.nextDouble();
+		System.out.println("Wow, you are " + intInput+ "?!");
+		
+		System.out.println("That's cool! How many pets do you have?");
+		int dogInput = readKeyboardInput.nextInt();
+		System.out.println("I wish I had " + dogInput + " pets!");
+		
+		System.out.println("What school do you go to?");
+		String schoolInput = readKeyboardInput.nextLine();
+		System.out.println("I bet " + schoolInput + " is a cool school!");
+		//Doesn't have a space to let you type in your school name!
+		
+		System.out.println("What is your favorite color?");
+		String colorInput = readKeyboardInput.nextLine();
+		System.out.println(colorInput + " is my favorite too!");
 	}
 }
